9c3fdcd828d66632dd730dab9243b465eb020a28
GuilhermeBley <guilhermecbley@gmail.com>
2023-01-13 12:56:43 -0300

    Core - Security - Claims #15

---
+++ src/BlSpent.Core/Security/PageClaims.cs
@@ -0,0 +1,29 @@
+using System.Security.Claims;
+
+namespace BlSpent.Core.Security;
+
+/// <summary>
+/// Page claims
+/// </summary>
+public static class PageClaims
+{
+    /// <summary>
+    /// PageRule
+    /// </summary>
+    public const string Type = "PageRule";
+
+    /// <summary>
+    /// UserClaim | Owner
+    /// </summary>
+    public static Claim Owner { get; } = new Claim(Type, "Owner");
+
+    /// <summary>
+    /// UserClaim | ReadOnly
+    /// </summary>
+    public static Claim ReadOnly { get; } = new Claim(Type, "ReadOnly");
+
+    /// <summary>
+    /// UserClaim | Modifier
+    /// </summary>
+    public static Claim Modifier { get; } = new Claim(Type, "Modifier");
+}
