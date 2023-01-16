f6f678799185342febc70af7009cf9fdef89f098
GuilhermeBley <guilhermecbley@gmail.com>
2023-01-16 14:01:01 -0300

    Application - repository to GuestPage #23

--- src/BlSpent.Application/Repository/IGuestPageRepository.cs
+++ src/BlSpent.Application/Repository/IGuestPageRepository.cs
@@ -5,6 +5,6 @@ namespace BlSpent.Application.Repository;
 
 public interface IGuestPageRepository : IBaseRepository<GuestPageModel, GuestPage, Guid>
 {
-    Task<CostModel?> GetByUserOrDefault(Guid userId);
-    IAsyncEnumerable<CostModel> GetByPage(Guid pageId);
+    IAsyncEnumerable<GuestPageModel> GetByUserOrDefault(Guid userId);
+    IAsyncEnumerable<GuestPageModel> GetByPage(Guid pageId);
 }
