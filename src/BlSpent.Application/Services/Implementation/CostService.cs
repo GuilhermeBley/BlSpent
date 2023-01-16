098c364eda60139e459ecdd988bfbeab3b1ae179
Guilherme Bley <guilhermecbley@gmail.com>
2023-01-15 23:28:18 -0300

    Application - service to Costs #13

--- src/BlSpent.Application/Services/Implementation/CostService.cs
+++ src/BlSpent.Application/Services/Implementation/CostService.cs
@@ -1,11 +1,22 @@
 using BlSpent.Application.Model;
 using BlSpent.Core.Entities;
 using BlSpent.Application.Services.Interfaces;
+using BlSpent.Application.UoW;
+using BlSpent.Application.Repository;
 
 namespace BlSpent.Application.Services.Implementation;
 
 public class CostService : ICostService
 {
+    private readonly IUnitOfWork _uow;
+    private readonly ICostRepository _costRepository;
+
+    public CostService(IUnitOfWork uow, ICostRepository costRepository)
+    {
+        _uow = uow;
+        _costRepository = costRepository;
+    }
+
     public Task<CostModel> Add(Cost entity)
     {
         throw new NotImplementedException();
