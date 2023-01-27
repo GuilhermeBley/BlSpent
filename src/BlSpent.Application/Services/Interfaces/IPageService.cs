327e2c1246d3f2221a5d07a02235d297792242c4
GuilhermeBley <guilhermecbley@gmail.com>
2023-01-27 15:19:16 -0300

    Application - service to Page #24 - interface

--- src/BlSpent.Application/Services/Interfaces/IPageService.cs
+++ src/BlSpent.Application/Services/Interfaces/IPageService.cs
@@ -4,7 +4,7 @@ namespace BlSpent.Application.Services.Interfaces;
 
 public interface IPageService
 {
-    Task<IEnumerable<PageModel>> GetAllByUser(Guid userId);
+    IAsyncEnumerable<PageModel> GetAllByUser(Guid userId);
     Task<PageModel?> Create(PageModel pageModel);
     Task<PageModel?> Update(Guid pageId, PageModel pageModel);
     Task<PageModel?> Delete(Guid pageId);
