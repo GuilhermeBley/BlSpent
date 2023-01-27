f21e07c4727de29052c2542b44004c7460d642fa
GuilhermeBley <guilhermecbley@gmail.com>
2023-01-27 14:29:52 -0300

    security from services #42 - add base service

---
+++ src/BlSpent.Application/Services/Implementation/BaseService.cs
@@ -0,0 +1,15 @@
+using BlSpent.Application.Security;
+using BlSpent.Application.Security.Internal;
+
+namespace BlSpent.Application.Services.Implementation;
+
+public abstract class BaseService
+{
+    private readonly ISecurityContext _securityContext;
+    internal readonly ClaimModelChecker _securityChecker;
+    public BaseService(ISecurityContext securityContext)
+    {
+        _securityContext = securityContext;
+        _securityChecker = new ClaimModelChecker(_securityContext);
+    }
+}
