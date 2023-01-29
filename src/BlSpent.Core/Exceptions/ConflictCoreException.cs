0864b4906280bad2c6217845d69a4fa5e4e399b8
GuilhermeBley <guilhermecbley@gmail.com>
2023-01-29 09:42:02 -0300

    Core - Conflict exception #44

---
+++ src/BlSpent.Core/Exceptions/ConflictCoreException.cs
@@ -0,0 +1,13 @@
+namespace BlSpent.Core.Exceptions;
+
+/// <summary>
+/// Represets a exception with status code <see cref="System.Net.HttpStatusCode.Conflict"/>
+/// </summary>
+public sealed class ConflictCoreException : CoreException
+{
+    public const string DefaultMessage = "Conflict";
+    public override int StatusCode => (int)System.Net.HttpStatusCode.Conflict;
+    public ConflictCoreException(string? message = DefaultMessage) : base(message)
+    {
+    }
+}
