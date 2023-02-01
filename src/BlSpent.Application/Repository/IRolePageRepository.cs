387d7ce8f16878e103869806e3f3b73103401f43
GuilhermeBley <guilhermecbley@gmail.com>
2023-01-31 22:41:16 -0300

    Application - service to RolePage #25 - repair repository role

--- src/BlSpent.Application/Repository/IRolePageRepository.cs
+++ src/BlSpent.Application/Repository/IRolePageRepository.cs
@@ -8,6 +8,5 @@ public interface IRolePageRepository : IBaseRepository<RolePageModel, RolePage, 
     IAsyncEnumerable<RolePageModel> GetByUserOrDefault(Guid userId);
     IAsyncEnumerable<RoleUserPageModel> GetByPage(Guid pageId);
     Task<RoleUserPageModel?> GetRoleUserByIdOrDefault(Guid rolePageId);
-    Task<RolePageModel?> CreateOrUpdate(Guid id, RolePageModel rolePageModel);
     Task<RoleUserPageModel?> GetByPageAndUserOrDefault(Guid userId, Guid pageId);
 }
