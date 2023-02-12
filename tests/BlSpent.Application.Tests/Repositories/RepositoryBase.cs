372054fa46adde80199f72cf64ea71b3cccded70
GuilhermeBley <guilhermecbley@gmail.com>
2023-02-11 22:49:53 -0300

    Application.Tests create tests to services #50 - repositories - userrepository

--- tests/BlSpent.Application.Tests/Repositories/RepositoryBase.cs
+++ tests/BlSpent.Application.Tests/Repositories/RepositoryBase.cs
@@ -6,7 +6,7 @@ namespace BlSpent.Application.Tests.Repositories;
 internal abstract class RepositoryBase
 {
     protected readonly AppDbContext _context;
-    private readonly IMapper _mapper;
+    protected readonly IMapper _mapper;
 
     public RepositoryBase(
         AppDbContext contex,
