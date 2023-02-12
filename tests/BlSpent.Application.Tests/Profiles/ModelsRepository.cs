a147979395765cbb53e8181dda54f98b12722490
GuilhermeBley <guilhermecbley@gmail.com>
2023-02-11 22:59:44 -0300

    Application.Tests create tests to services #50 - map entities

--- tests/BlSpent.Application.Tests/Profiles/ModelsRepository.cs
+++ tests/BlSpent.Application.Tests/Profiles/ModelsRepository.cs
@@ -29,5 +29,24 @@ public class ModelsRepository : Profile
                 opt => opt.MapFrom(src => new Smartec.Validations.BaseDate(src.EntryBaseDate))
             );
         CreateMap<Application.Model.GoalModel, Models.GoalModel>().ReverseMap();
+
+        CreateMap<Core.Entities.User, Models.UserModel>()
+            .ForMember(
+                dest => dest.Salt,
+                opt => opt.MapFrom(src => src.PasswordSalt)
+            );
+        CreateMap<Core.Entities.Page, Models.PageModel>();
+        CreateMap<Core.Entities.RolePage, Models.RolePageModel>();
+        CreateMap<Core.Entities.Cost, Models.CostModel>()
+            .ForMember(
+                dest => dest.EntryBaseDate,
+                opt => opt.MapFrom(src => src.EntryBaseDate.ToDateTime())
+            );
+        CreateMap<Core.Entities.Earning, Models.EarningModel>()
+            .ForMember(
+                dest => dest.EntryBaseDate,
+                opt => opt.MapFrom(src => src.EntryBaseDate.ToDateTime())
+            );
+        CreateMap<Core.Entities.Goal, Models.GoalModel>();
     }
 }
