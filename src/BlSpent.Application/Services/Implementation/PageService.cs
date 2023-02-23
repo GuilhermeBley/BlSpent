ded361fc3b43ef1107bd60bedfe5eac46dae9669
Guilherme Bley <guilhermecbley@gmail.com>
2023-02-22 22:15:25 -0300

    Application.Tests create tests to services #50 - page tests

--- src/BlSpent.Application/Services/Implementation/PageService.cs
+++ src/BlSpent.Application/Services/Implementation/PageService.cs
@@ -23,7 +23,7 @@ public class PageService : BaseService, IPageService
         _uoW = uoW;
     }
 
-    public async Task<PageModel?> Create(PageModel pageModel)
+    public async Task<PageModel> Create(PageModel pageModel)
     {
         await _securityChecker.ThrowIfIsntLogged();
 
--- src/BlSpent.Application/Services/Interfaces/IPageService.cs
+++ src/BlSpent.Application/Services/Interfaces/IPageService.cs
@@ -4,7 +4,7 @@ namespace BlSpent.Application.Services.Interfaces;
 
 public interface IPageService
 {
-    Task<PageModel?> Create(PageModel pageModel);
+    Task<PageModel> Create(PageModel pageModel);
     Task<PageModel?> CurrentPageGetByIdOrDefault(Guid id);
     Task<PageModel> CurrentPageUpdate(Guid pageId, PageModel pageModel);
     Task<PageModel> CurrentPageRemove(Guid pageId);
