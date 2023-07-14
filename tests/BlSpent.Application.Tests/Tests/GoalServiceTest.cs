f7074de26a8ccbc4250a0bd578bd0dcb05a08bff
Guilherme Bley <guilhermecbley@gmail.com>
2023-07-13 23:21:05 -0300

    Create test GoalService #70

--- tests/BlSpent.Application.Tests/Tests/GoalServiceTest.cs
+++ tests/BlSpent.Application.Tests/Tests/GoalServiceTest.cs
@@ -22,4 +22,103 @@ public class GoalServiceTest : BaseTest
                 GoalMock.CreateValidRandomGoal(userTupleCreated.Page.Id)
         ));
     }
+
+    [Fact]
+    public async Task CreateValidRandomGoal_TryCreateWithoutLogin_FailedForbbiden()
+    {
+        var provider = ServiceProvider;
+
+        var goalService = provider.GetRequiredService<IGoalService>();
+
+        var userTupleCreated = await CreatePageAndUser(serviceProvider: provider);
+
+        await Assert.ThrowsAnyAsync<Core.Exceptions.ForbiddenCoreException>(
+            () => goalService.Add(
+                GoalMock.CreateValidRandomGoal(userTupleCreated.Page.Id)
+        ));
+    }
+
+    [Fact]
+    public async Task GetByPageId_TryGetByPage_Success()
+    {
+        var provider = ServiceProvider;
+
+        var goalService = provider.GetRequiredService<IGoalService>();
+
+        var userTupleCreated = await CreatePageAndUser(serviceProvider: provider);
+
+        using var ctx = CreateContext(userTupleCreated.User, userTupleCreated.Role);
+
+        await goalService.Add(
+                GoalMock.CreateValidRandomGoal(userTupleCreated.Page.Id));
+
+        Assert.NotEmpty(
+            await goalService.GetByPageId(userTupleCreated.Page.Id).ToListAsync()
+        );
+    }
+
+    [Fact]
+    public async Task GetByPageId_TryGetByPageWithoutLogin_FailedForbbiden()
+    {
+        var provider = ServiceProvider;
+
+        var goalService = provider.GetRequiredService<IGoalService>();
+
+        var userTupleCreated = await CreatePageAndUser(serviceProvider: provider);
+
+        await Assert.ThrowsAnyAsync<Core.Exceptions.ForbiddenCoreException>(
+            () => goalService.GetByPageId(userTupleCreated.Page.Id).ToListAsync().AsTask()
+        );
+    }
+
+    [Fact]
+    public async Task UpdateByIdOrDefault_TryUpdateNonExistent_FailedToUpdate()
+    {
+        var provider = ServiceProvider;
+
+        var goalService = provider.GetRequiredService<IGoalService>();
+
+        var userTupleCreated = await CreatePageAndUser(serviceProvider: provider);
+
+        using var ctx = CreateContext(userTupleCreated.User, userTupleCreated.Role);
+
+        await Assert.ThrowsAnyAsync<Core.Exceptions.NotFoundCoreException>(
+            () => goalService.UpdateByIdOrDefault(Guid.NewGuid(), GoalMock.CreateValidRandomGoal(Guid.NewGuid()))
+        );
+    }
+
+    [Fact]
+    public async Task UpdateByIdOrDefault_TryUpdateWithoutLogin_FailedForbbiden()
+    {
+        var provider = ServiceProvider;
+
+        var goalService = provider.GetRequiredService<IGoalService>();
+
+        var userTupleCreated = await CreatePageAndUser(serviceProvider: provider);
+
+        await Assert.ThrowsAnyAsync<Core.Exceptions.NotFoundCoreException>(
+            () => goalService.UpdateByIdOrDefault(userTupleCreated.Page.Id, GoalMock.CreateValidRandomGoal(Guid.NewGuid()))
+        );
+    }
+
+    [Fact]
+    public async Task UpdateByIdOrDefault_TryUpdate_Success()
+    {
+        var provider = ServiceProvider;
+
+        var goalService = provider.GetRequiredService<IGoalService>();
+
+        var userTupleCreated = await CreatePageAndUser(serviceProvider: provider);
+
+        using var ctx = CreateContext(userTupleCreated.User, userTupleCreated.Role);
+
+        var goalCreated = await goalService.Add(
+                GoalMock.CreateValidRandomGoal(userTupleCreated.Page.Id));
+
+        Assert.NotNull(
+            await goalService.UpdateByIdOrDefault(
+                goalCreated.Id,
+                GoalMock.CreateValidRandomGoal(userTupleCreated.Page.Id))
+        );
+    }
 }
